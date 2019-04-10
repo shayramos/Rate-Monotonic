9f2a53510d88bc5ab9f97ef23ce4c636e3d994c2
Shay Ramos <shayramos@hotmail.com>
2019-04-09 22:44:22 -0300

    iniciando repo

---
+++ processos.py
@@ -0,0 +1,9 @@
+class Process:
+    def __init__(self):
+        print("This is the constructor method.")
+
+    def excutar(self):
+        print("Executando")
+
+    def emEspera(self):
+        print("Em espera")
