a08b8694a2c0cb9bf5a4d78f20a0fdf34f9b8444
Shay Ramos <shayramos@hotmail.com>
2019-04-12 01:37:10 -0300

    ainda sem funcionar

--- RM_algorithm.py
+++ RM_algorithm.py
@@ -1,3 +1,6 @@
+import math
+from decimal import Decimal
+
 class RateMonotonic:
 
     def isFactive(self, listObj):
@@ -5,20 +8,20 @@ class RateMonotonic:
             if elem is not listObj[0]:
                 last_response_time = elem.executionTime
                 while True:
-                    response = self.calculate_response(last_response_time, listObj)
-                    if response[0] == response[1]:
-                        if response[0] > int(elem.deadline):
+                    response = self.calculate_response(last_response_time, listObj, elem.executionTime)
+                    if response == int(last_response_time):
+                        if response > int(elem.deadline):
                             return False
                         else:
                             return True
                     else:
-                        last_response_time = response[1]
+                        last_response_time = response
 
     @staticmethod
-    def calculate_response(last_response_time, listObj):
+    def calculate_response(last_response_time, listObj, custo):
         response_time = 0
         for i in listObj:
-            response_time += (int(last_response_time) / int(i.periodo)) * int(i.executionTime)
-        response_time = int(response_time) + int(last_response_time)
-        last_response_time = response_time
-        return response_time, last_response_time
+            response_time += (float(last_response_time) / float(i.periodo)) * float(i.executionTime)
+            response_time = math.ceil(response_time)
+        response_time = int(response_time) + int(custo)
+        return response_time
